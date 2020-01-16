d15a3fb0f00b3eb1a2b61bb7ccadcb40a0bf08b9
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-16 11:56:17 +0100

    testing git

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+        // This is the theme of your application.Test
         //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, 
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
